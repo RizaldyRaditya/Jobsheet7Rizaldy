e0b24254e39939332aa43ae278a5003e2f22896f
Rudi Erwanto <Rudi Erwanto@192.168.0.4>
2018-08-03 19:25:13 +0700

    Coba IF

--- src/Latihan/CobaPercobaan.java
+++ src/Latihan/CobaPercobaan.java
@@ -5,6 +5,8 @@
  */
 package Latihan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rudi Erwanto
@@ -14,5 +16,19 @@ public class CobaPercobaan {
         String identitas = "Rizaldy Raditya Erwanto / XRPL 5 / 36";
         System.out.println("Identitas : "+ identitas);
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variabel a : "+ a);
+            System.out.println("nilai a lebih dari 5");
+        }
     }
 }
