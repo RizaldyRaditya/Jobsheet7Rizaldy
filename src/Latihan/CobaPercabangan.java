177578b852e075131b52582fa41deafe26de949b
Rudi Erwanto <Rudi Erwanto@192.168.0.4>
2018-08-03 18:57:41 +0700

    Initial Commit

---
+++ src/Latihan/CobaPercabangan.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class CobaPercabangan {
+    public static void main(String[] args) {
+        String identitas = "Rizaldy Raditya Erwanto / XRPL 5 / 36";
+        System.out.println("Identitas : "+ identitas);
+    }
+}
