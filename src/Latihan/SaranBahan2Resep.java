8a5f5fb5a2cbf654196ae1c9b4175d421226260f
Rudi Erwanto <Rudi Erwanto@192.168.0.4>
2018-08-03 19:54:44 +0700

    Initial Commit

---
+++ src/Latihan/SaranBahan2Resep.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+        String identitas = "Rizaldy Raditya Erwanto / XRPL 5 / 36";
+        System.out.println("Identitas : "+ identitas);
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+    }
+}
